3757b3efee80015b067c63e875c98ead65d33c1b
Skyd00x <andrea-negro@outlook.fr>
2024-05-24 12:06:29 +0200

    Analyse de  besoins et création exo 8

---
+++ 2024_BDD_NEGRO_ANDREA_EX8/2024_BDD_NEGRO_ANDREA_EX8.sql
@@ -0,0 +1,22 @@
+SELECT DISTINCT Utilisateurs.*
+FROM Utilisateurs
+JOIN Reservations ON Utilisateurs.NumeroEtudiant = Reservations.NumeroEtudiant;
+
+
+SELECT Materiel.*
+FROM Materiel
+LEFT JOIN Reservations ON Materiel.IdMatériel = Reservations.IdMatériel
+WHERE Reservations.IdMatériel IS NULL;
+
+
+SELECT Materiel.*, COUNT(*) as NombreEmprunts
+FROM Materiel
+JOIN Reservations ON Materiel.IdMatériel = Reservations.IdMatériel
+GROUP BY Materiel.IdMatériel
+HAVING COUNT(*) > 3;
+
+
+SELECT Utilisateurs.*, COUNT(*) as NombreEmprunts
+FROM Utilisateurs
+JOIN Reservations ON Utilisateurs.NumeroEtudiant = Reservations.NumeroEtudiant
+GROUP BY Utilisateurs.NumeroEtudiant;
